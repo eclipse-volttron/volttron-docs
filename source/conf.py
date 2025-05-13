5b560766bb868b12f5b678b764a2745b3fa0f124
chandrika <chandrika@pnnl.gov>
2025-05-13 12:20:56 -0700

    updates for 9.0.4 branch

--- source/conf.py
+++ source/conf.py
@@ -40,9 +40,9 @@ copyright = '2022, The VOLTTRON Community'
 author = 'The VOLTTRON Community'
 
 # The short X.Y version
-version = '9.0'
+version = '9.0.4'
 # The full version, including alpha/beta/rc tags
-release = '9.0'
+release = '9.0.4'
 
 
 # -- General configuration ---------------------------------------------------
@@ -232,7 +232,7 @@ vagent_readme = os.path.join(script_dir, "volttron-api")
 def generate_volttron_docs(app):
     import shutil
     global script_dir, vdocs_root, vdocs_source_subdir, vdocs_source_abs_path, vagent_readme
-    volttron_version = "releases/9.0"
+    volttron_version = "releases/9.0.4"
     volttron_repo = "https://github.com/volttron/volttron"
 
     subprocess.check_call(["git", "clone", "--no-checkout", volttron_repo, vdocs_root], cwd=script_dir)
